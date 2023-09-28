c60a7a9cbafc504fe8e6dd4cd18755507bb76be8
Maxime <maxrofra@gmail.com>
2023-09-28 11:49:34 +0200

    Changed game config to be in games/ directory

--- source/utils/config.lua
+++ source/utils/config.lua
@@ -1,13 +1,16 @@
 import "playdate"
 
+dirPathGames = "games/"
+
 function loadGameConfig(name) 
+	local dirPathGame = dirPathGames..name
 	-- Check game folder exists
-	if not (playdate.file.exists(name) and playdate.file.isdir(name)) then
+	if not (playdate.file.exists(dirPathGame) and playdate.file.isdir(dirPathGame)) then
 		return nil
 	end
 	
-	local filePathGameConfig = name.."/game.json"
-	local filePathItemsConfig = name.."/items.json"
+	local filePathGameConfig = dirPathGame.."/game.json"
+	local filePathItemsConfig = dirPathGame.."/items.json"
 	
 	-- Check game files exist
 	if not (playdate.file.exists(filePathGameConfig) and playdate.file.exists(filePathItemsConfig)) then
